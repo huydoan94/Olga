f0fb017244e34b13a5d853156feaca7e597e7a6f
Dang Huy Doan <danghuy1994@gmail.com>
2022-08-03 14:07:40 -0600

    Add comments

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 import path from 'path';
 import spawn from 'cross-spawn';
 
+// To terminate the process if something interrupt it
 process.on("SIGTERM", () => process.exit(1));
 process.on("SIGINT", () => process.exit(1));
 process.on("SIGHUP", () => process.exit(1));
 
+// Get current working folder, to determine the location of modules.
 const currentWorkingFolder = process.cwd();
+// Get executable folders
 const executablesPath = path.join(currentWorkingFolder, 'node_modules/.bin/gauge');
 
+// Get arguments and run
 const args = process.argv.slice(2);
 const gauge = spawn(executablesPath, ['run', ...args]);
 
+// Print message
 gauge.stdout.on("data", data => {
     console.log(`${data}`);
 });
