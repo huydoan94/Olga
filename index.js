d458842f77ecad88005c7cff945ac7ed4c35414e
Dang Huy Doan <danghuy1994@gmail.com>
2022-08-03 12:02:43 -0600

    First commit of CLI

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import path from 'path';
+import { spawn } from 'child_process';
 
-console.log("Hello, hello");
+const gauge = spawn('gauge', ['run', 'specs']);
+
+gauge.stdout.on("data", data => {
+    console.log(`${data}`);
+});
+
+gauge.stderr.on("data", data => {
+    console.log(`${data}`);
+});
+
+gauge.on('error', (error) => {
+    console.log(`Error: ${error.message}`);
+});
+
+gauge.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
